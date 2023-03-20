bc7acc7871ce404816003f5fca457367fedde079
Suam Kang <ksa199653@gmail.com>
2023-03-20 21:18:50 +0900

    필요없는 파일 삭제후, dev브렌치로 수정한 파일들 커밋

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,29 +1,29 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { FaHamburger } from "react-icons/fa";
+
 import styled from "styled-components";
 import Template from "../component/Template";
 
-// 더미 데이터
-const dummyData = [
-  {
-    id: 1,
-    title: "할일1",
-    checked: false,
-  },
-  {
-    id: 2,
-    title: "할일2",
-    checked: false,
-  },
-  {
-    id: 3,
-    title: "할일3",
-    checked: false,
-  },
-];
-
 // 이부분은 todo페이지의 App.js같은 메인컴포넌트이다
 const Todo = () => {
+  // 더미 데이터
+  const dummyData = [
+    {
+      id: 1,
+      title: "할일1",
+      checked: false,
+    },
+    {
+      id: 2,
+      title: "할일2",
+      checked: false,
+    },
+    {
+      id: 3,
+      title: "할일3",
+      checked: false,
+    },
+  ];
+
   // 앞으로 추가 할일 인풋 상태
   const [inputTodo, setInputTodo] = useState("");
   // 할일들
@@ -31,10 +31,13 @@ const Todo = () => {
 
   return (
     <div className="App">
-      <div className="headers">
-        <span>오늘 할일</span>
-      </div>
-      <Template  className="todo-template" todos={todos} setTodos={setTodos} inputTodo={inputTodo} setInputTodo={setInputTodo}/>
+      <Template
+        className="todo-template"
+        todos={todos}
+        setTodos={setTodos}
+        inputTodo={inputTodo}
+        setInputTodo={setInputTodo}
+      />
     </div>
   );
 };
--- src/component/TodoInsert.js
+++ src/component/TodoInsert.js
@@ -1,42 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-
 // 투두 인풋창
-const TodoInsert = ({todos, setTodos, inputTodo, setInputTodo }) => {
-
-
-    // 할일 추가버튼 클릭 이벤트
-    const handleInputClick = () => {
-      // 새로 추가할 todo
-      const newTodo = {
-        id: todos.length + 1,
-        title: inputTodo,
-        checked: false,
-      };
-      setTodos([...todos, newTodo]);
+const TodoInsert = ({ todos, setTodos, inputTodo, setInputTodo }) => {
+  // 할일 추가버튼 클릭 이벤트
+  const handleInputClick = () => {
+    // 새로 추가할 todo
+    const newTodo = {
+      id: todos.length + 1,
+      title: inputTodo,
+      checked: false,
     };
+    setTodos([...todos, newTodo]);
+  };
 
-    return(
-        <>
-          <div className="todo-insert">
-          <form action="#" name="todo_insert">
-            <input
-              type="text"
-              placeholder="여기에 할일을 입력하세요"
-              value={inputTodo}
-              autoFocus
-              onChange={(e)=>{
-                e.preventDefault();
-                const { value } = e.target;
-                setInputTodo(value);
-              }}
-            />
-          </form>
-          <button onClick={handleInputClick}>추가</button>
-        </div>
-        </>
-    );
+  return (
+    <div style={{display:"flex" , justifyContent : "center"}} >
+      <div>오늘할일</div>
+      <div className="todo-insert" style={{display:"flex" , justifyContent : "center"}}>
+        <form action="#" name="todo_insert">
+          <input
+            type="text"
+            placeholder="여기에 할일을 입력하세요"
+            value={inputTodo}
+            autoFocus
+            onChange={(e) => {
+              e.preventDefault();
+              const { value } = e.target;
+              setInputTodo(value);
+            }}
+          />
+        </form>
+        <button onClick={handleInputClick}>추가</button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoInsert;
