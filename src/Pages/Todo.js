d2773294a9b858a24db1cec4829485452863683e
Suam Kang <ksa199653@gmail.com>
2023-03-23 00:54:05 +0900

    스타일 적용(styled component)후 기본기능구현 완료

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,42 +1,88 @@
 import React, { useState } from "react";
-import Template from "../component/Template";
+import styled from "styled-components";
+import TodoList from "../component/TodoList";
+import TodoInsert from "../component/TodoInsert";
+import TodoHead from "../component/TodoHead";
+import '../App.css'
 
-// 이부분은 todo페이지의 App.js같은 메인컴포넌트이다
+// todo전체 레이아웃
+const TodoTemplate = styled.div`
+  width: 520px;
+  height: 750px;
+  position: relative;
+  background: #fff;
+  border-radius: 15px;
+  box-shadow: 0 2 5 rgba(0,0,0, .05);
+
+  margin: 96px auto 32px;
+  display: flex; 
+  flex-direction: column;
+`;
+
 const Todo = () => {
-  // 더미 데이터
-  const dummyData = [
-    {
-      id: 1,
-      title: "할일1",
-      checked: false,
-    },
-    {
-      id: 2,
-      title: "할일2",
-      checked: false,
-    },
-    {
-      id: 3,
-      title: "할일3",
-      checked: false,
-    },
-  ];
-
   // 앞으로 추가 할일 인풋 상태
   const [inputTodo, setInputTodo] = useState("");
   // 할일들
-  const [todos, setTodos] = useState(dummyData);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "운동하기",
+      done: false,
+    },
+    {
+      id: 2,
+      text: "빨래하기",
+      done: false,
+    },
+    {
+      id: 3,
+      text: "리액트 공부하기",
+      done: false,
+    },
+  ]);
 
+  // inputTodo 인풋 이벤트 받기
+  const onChange = (e) => {
+    e.preventDefault();
+    const {value} = e.target;
+    setInputTodo(value);
+  };  
+
+  // 할일 추가 이벤트
+  const onCreate = () => {
+    // 새로 추가할 todo
+    const newTodo = {
+      id: todos.length + 1,
+      text: inputTodo,
+      done: false,
+    };
+    setTodos([...todos, newTodo]);
+    setInputTodo(""); // 인풋창 초기화
+  };
+
+  // 할일 선택 항목 삭제 이벤트
+  const onRemove = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  // 할일 선택 항목 수정 이벤트
+  const onToggle = (id) => {
+    setTodos(todos.map( todo => todo.id === id ? {...todo, done : !todo.done} : todo));
+  }
+
+  // 남은 할일 개수
+  const undoneTasks = () => {
+    setTodos(todos.filter(todo => !todo.done));
+  }
+  console.log(()=> undoneTasks) //??
+
+ // 기존 Template 컴포넌트 삭제(불필요한 컴포넌트였음)
   return (
-    <div className="App">
-      <Template
-        className="todo-template"
-        todos={todos}
-        setTodos={setTodos}
-        inputTodo={inputTodo}
-        setInputTodo={setInputTodo}
-      />
-    </div>
+    <TodoTemplate>
+      <TodoHead undoneTasks={()=>undoneTasks()} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      <TodoInsert inputTodo={inputTodo} setInputTodo={setInputTodo} onCreate={onCreate} onChange={onChange}/>
+    </TodoTemplate>
   );
 };
 
